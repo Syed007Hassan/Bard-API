137b4c3f8e70e7b2a70abb7520ca27d41379fa9a
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-13 22:21:42 +0500

    key changed

--- bard2.py
+++ bard2.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()  # Load the environment variables from the .env file
 
-api_key = os.getenv('BARD_API_KEY1')  # Retrieve the API key from the environment variables
+api_key = os.getenv('BARD_API_KEY')  # Retrieve the API key from the environment variables
 
 while input != "q":
     question = input("Enter Prompt: ")
