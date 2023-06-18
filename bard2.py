f68b39ca9bdf4af9d12aa3c1b82f6f35fac41ee7
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-19 00:23:10 +0500

    prompts for your own data

--- bard2.py
+++ bard2.py
@@ -4,8 +4,32 @@ import os
 
 load_dotenv()  # Load the environment variables from the .env file
 
-api_key = os.getenv('BARD_API_KEY')  # Retrieve the API key from the environment variables
+# Retrieve the API key from the environment variables
+api_key = os.environ.get('BARD_API_KEY')
 
-while input != "q":
-    question = input("Enter Prompt: ")
-    print(Bard(api_key).get_answer(question)['content'])
+# create a list of data
+
+data = [
+    {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+
+    },
+    {
+        "name": "Peter",
+        "age": 40,
+        "city": "Boston"
+    }
+]
+
+while True:
+    prompt = input("Enter the prompt: ")
+    if prompt == "q":
+        break
+    prompt2 = "From the data list provided at the end, your response should not be in JSON format and it should be concise."
+    # Use Bard to get an answer to the prompt
+    answer = Bard(api_key).get_answer(
+        prompt2 + str(data) + prompt)['content']
+    # Print the answer
+    print(answer)
