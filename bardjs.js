038345e21808966bc1f6e36898f0b478dc6ed144
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-06-19 02:09:03 +0500

    js for bard

---
+++ bardjs.js
@@ -0,0 +1,31 @@
+import Bard, { askAI } from "bard-ai";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Retrieve the API key from the environment variables
+const apiKey = process.env.BARD_API_KEY;
+
+await Bard.init(apiKey);
+
+// Create a list of data
+const data = [
+    {
+        name: 'John',
+        age: 30,
+        city: 'New York'
+    },
+    {
+        name: 'Peter',
+        age: 40,
+        city: 'Boston'
+    }
+];
+
+
+
+const prompt2 = 'From the data list provided at the end, your response should not be in JSON format and it should be concise.';
+
+ console.log(await Bard.askAI("find all the names whose age is greater than 30 from the given data list: " +  data ));
+
